3b1f21613e34602ada5896b7b52933b9430775bd
Arno Dirlam <arnodirlam@googlemail.com>
2019-12-11 22:35:20 +0100

    Add brew-setup.sh

---
+++ brew-setup.sh
@@ -0,0 +1,12 @@
+xcode-select --install
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install autoconf gnupg openssl
+brew install ansible cask git-crypt htop jq rsync watch wget
+
+brew cask install 1password battle-net caffeine chromedriver clipmenu deckset diffmerge
+brew cask install firefox flash-npapi flux google-chrome iterm2 karabiner-elements keepassx
+brew cask install macmediakeyforwarder notion slack spectacle spotify synology-cloud-station-drive
+brew cask install the-unarchiver toggldesktop veracrypt visual-studio-code vlc whatsapp
+
