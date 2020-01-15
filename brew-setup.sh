5b068e6abd3292f623cb3c2ac8523cc1e3249605
Arno Dirlam <arnodirlam@googlemail.com>
2020-01-15 17:30:10 +0100

    Add git and GPG setup

--- brew-setup.sh
+++ brew-setup.sh
@@ -2,11 +2,13 @@ xcode-select --install
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew install autoconf gnupg openssl
+brew install autoconf gnupg openssl pinentry-mac
 brew install ansible cask git-crypt htop jq rsync watch wget
 
 brew cask install 1password battle-net caffeine chromedriver clipmenu deckset diffmerge
 brew cask install firefox flash-npapi flux google-chrome iterm2 karabiner-elements keepassx
 brew cask install macmediakeyforwarder notion slack spectacle spotify synology-cloud-station-drive
-brew cask install the-unarchiver toggldesktop veracrypt visual-studio-code vlc whatsapp
+brew cask install the-unarchiver toggldesktop vanilla veracrypt visual-studio-code vlc whatsapp
 
+echo "pinentry-program $(brew --prefix)/bin/pinentry-mac" >> ~/.gnupg/gpg-agent.conf
+gpgconf --kill gpg-agent
---
+++ git-setup.sh
@@ -0,0 +1,9 @@
+git config --global user.name "Arno Dirlam"
+git config --global user.email "arnodirlam@googlemail.com"
+git config --global user.signingkey 87970B680299992D
+git config --global commit.gpgsign true
+
+git config --global push.default simple
+git config --global --bool pull.rebase true
+
+gpg --list-secret-keys --keyid-format LONG
