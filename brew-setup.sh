f6f92c4da37526aa49d1b3c6d7bec1603bf9753d
Arno Dirlam <arnodirlam@googlemail.com>
2025-01-05 14:22:25 +0100

    Update brew-setup.sh

--- brew-setup.sh
+++ brew-setup.sh
@@ -2,13 +2,7 @@ xcode-select --install
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew install autoconf gnupg openssl pinentry-mac
-brew install ansible cask git-crypt htop jq rsync watch wget
-
-brew cask install 1password battle-net caffeine chromedriver clipmenu deckset diffmerge
-brew cask install firefox flash-npapi flux google-chrome iterm2 karabiner-elements keepassx
-brew cask install macmediakeyforwarder notion slack spectacle spotify synology-cloud-station-drive
-brew cask install the-unarchiver toggldesktop vanilla veracrypt visual-studio-code vlc whatsapp
+brew install 1password act actionlint asdf autoconf automake awscli bat bat-extras cmake coreutils curl direnv ffmpeg gh ghostscript git-lfs gitui glib gnu-sed gnupg gnutls graphite harfbuzz hcloud htop icu4c@76 imagemagick imagemagick@6 imap-backup jnv jpeg-xl jq libarchive libass libbluray libtool libxslt libyaml make mise neovim openjdk@11 openssl@3 pango pdftk-java pinentry-mac pkgconf qpdf readline ripgrep rust tesseract testdisk unixodbc unzip watch watchexec wget wxwidgets yamlfmt zsh-completions
 
 echo "pinentry-program $(brew --prefix)/bin/pinentry-mac" >> ~/.gnupg/gpg-agent.conf
 gpgconf --kill gpg-agent
